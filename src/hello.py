552033b52706bbb3e9290b8ccc388c4a468c2426
Liyi Huang <liyi.huang@tetrate.io>
2022-06-12 23:38:25 -0400

    fix doc

--- src/hello.py
+++ src/hello.py
@@ -1,10 +1,11 @@
+''' this is hello world python module '''
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    '''this is a hello world python'''
+    '''this is a hello world python function'''
     return 'Web App with Python Flask!!'
 
 app.run(host='0.0.0.0', port=8080)
