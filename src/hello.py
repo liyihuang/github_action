ef9cd892f805084f975bc1d1eaddac8d56b15e25
Liyi Huang <liyi.huang@tetrate.io>
2022-06-12 22:32:45 -0400

    test pylint

--- src/hello.py
+++ src/hello.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     '''this is a hello world python'''
-    return 'Web App with Python Flask!'
+    return 'Web App with Python Flask!!'
 
 app.run(host='0.0.0.0', port=8080)
