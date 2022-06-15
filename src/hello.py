92d5cefb442f01c1fa7ff6fdc1c1bf8e4dfbdbf2
Liyi Huang <liyi.huang@tetrate.io>
2022-06-15 17:03:30 -0400

    5:03pm

--- src/hello.py
+++ src/hello.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     '''this is a hello world python function'''
-    return 'hello ci cd pipeline 4:59pm'
+    return 'hello ci cd pipeline 5:05pm'
 
 app.run(host='0.0.0.0', port=8080)
